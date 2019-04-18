e23a52be89360876817480f0581c53c96e96a343
Justin Chao <justinchao20@gmail.com>
2019-04-18 17:00:03 -0400

    first commit

---
+++ RadiUs/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RadiUs
+//
+//  Created by Justin Chao on 4/18/19.
+//  Copyright © 2019 RadiUs Group 7. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
